84fcf36785192a7b76bb8847f2c76701fa030336
Cahangir2020 <61001888+Cahangir2020@users.noreply.github.com>
2020-02-20 12:19:53 +0300

    Add files via upload

---
+++ head.php
@@ -0,0 +1,30 @@
+<?php
+    include 'conf.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?=$site_title;?></title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?=$template_url;?>css/animate.css">
+    
+    <link rel="stylesheet" href="<?=$template_url;?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?=$template_url;?>css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?=$template_url;?>css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?=$template_url;?>css/aos.css">
+
+    <link rel="stylesheet" href="<?=$template_url;?>css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="<?=$template_url;?>css/flaticon.css">
+    <link rel="stylesheet" href="<?=$template_url;?>css/icomoon.css">
+    <link rel="stylesheet" href="<?=$template_url;?>css/style.css">
+  </head>
+  <body>
